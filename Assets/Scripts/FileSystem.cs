3851dbcce1f9450f9df113a24b4bbe2773081873
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2020-10-25 09:00:01 +0700

    add file system functionality

--- Assets/Scripts/FileSystem.cs
+++ Assets/Scripts/FileSystem.cs
@@ -4,5 +4,61 @@ using UnityEngine;
 
 public class FileSystem : MonoBehaviour
 {
-    public List<FileTree> Trees;
+    public List<FileTree> trees;
+    private List<FileNode> nodes;
+
+    private FileNode currentNode;
+    private List<FileNode> childNodes;
+
+    public void Start()
+    {
+        currentNode = null;
+        nodes = new List<FileNode>();
+        foreach (FileTree tree in trees)
+        {
+            nodes.AddRange(tree.theTree);
+        }
+    }
+
+    public bool OpenNode(string node_name)
+    {
+        for (int i = 0; i < childNodes.Count; i++) 
+        {
+            if (childNodes[i].name == node_name) 
+            {
+                currentNode = childNodes[i];
+                childNodes = new List<FileNode>();
+                foreach (FileNode node in nodes) 
+                {
+                    if (node.parentName == node_name)
+                    {
+                        childNodes.Add(node); 
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool OpenParentNode()
+    {
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            if (nodes[i].name == currentNode.parentName)
+            {
+                currentNode = nodes[i];
+                childNodes = new List<FileNode>();
+                foreach (FileNode node in nodes)
+                {
+                    if (node.parentName == currentNode.parentName)
+                    {
+                        childNodes.Add(node);
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
 }
